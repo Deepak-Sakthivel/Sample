37bb7031661541ef00de2b3b6b9dae2c4d2f5df2
Vignesh Balu <vignesh.balu@valuemomentum.com>
2023-09-26 19:59:12 +0530

    resolved stale element issue

--- ErieInsuranceMiniProject/src/main/java/PageObjectModel/LandingPage.java
+++ ErieInsuranceMiniProject/src/main/java/PageObjectModel/LandingPage.java
@@ -37,7 +37,9 @@ public class LandingPage {
 		for(WebElement type : insuranceType)
 		{
 			type.getText().contains(typeOfInsurance);
-			type.click();		
+			type.click();	
+			break;
+			
 		}
 				
 		/*
